230b04742dae0b00c44f8ca6542a3749d88dc59c
Krzyman134 <manka.krzysztof123@mail.com>
2023-09-19 23:54:24 +0200

    First script

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class Test : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject logo;
+    private float m_Speed;
+    private Vector2 myVector;
+    private bool up;
+
     void Start()
     {
-        
+        up = true;
+        m_Speed = 0.005f;
+        myVector = new Vector2(logo.transform.position.x, logo.transform.position.y);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(logo.transform.position.y > 3){
+             m_Speed = -0.005f;
+        }
+
+        if(logo.transform.position.y < -3){
+            m_Speed = 0.005f;
+        }
+
+        logo.transform.position = new Vector2(logo.transform.position.x, logo.transform.position.y + m_Speed);
     }
 }
